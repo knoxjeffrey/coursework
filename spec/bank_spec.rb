1c9fa76b2d8ff84577aca62f0073cc2fcaa9d20d
Bodacious <gavin@katanacode.com>
2014-12-16 20:29:26 +0000

    Added spec for Bank#initailize

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -2,5 +2,14 @@ require 'spec_helper'
 require 'bank'
 
 describe Bank do
+  
+  describe :initialize do
+    
+    it "should accept a numerical value and set the #bank attribute" do
+      expect(Bank.new(100).bank).to eql(100)
+    end
+    
+    
+  end
 
 end
