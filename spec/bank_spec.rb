c96e9f2d36d0c62c6bce4053005d45a02dd2ae59
Jeff Knox <knoxjeffrey@outlook.com>
2014-12-18 14:09:40 +0000

    new bank specs

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -9,7 +9,23 @@ describe Bank do
       expect(Bank.new(100).bank).to eql(100)
     end
     
-    
   end
-
+  
+  describe :deposit_money do
+    
+    it "should add the amount to the bank" do
+      bank = Bank.new(100)
+      bank.deposit_money(10)
+      expect(bank.bank).to eql(110)
+    end
+  end
+  
+  describe :withdraw_money do
+    
+    it "should add the amount to the bank" do
+      bank = Bank.new(100)
+      bank.withdraw_money(10)
+      expect(bank.bank).to eql(90)
+    end
+  end
 end
