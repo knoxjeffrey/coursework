232287f35ca1260b4fd0e58e555de9d4076ff575
Bodacious <gavin@katanacode.com>
2014-12-27 02:28:05 +0000

    documented my new classes and methods

--- app/acts_as_card_holder.rb
+++ app/acts_as_card_holder.rb
@@ -1,25 +1,40 @@
+# This module replaces the Hand and HandTotal modules from before.
+# Including this module in a class gives it the "CardHolder" duck-type; a user who may
+# hold and play with Cards.
 module ActsAsCardHolder
   
+  # The total hand value required to win the Game
   BLACKJACK_AMOUNT = 21
+
+  attr_writer :cards_held
   
-  attr_accessor :cards_held
-    
-  def initialize(*)
-    @cards_held = []  
+  # The Cards held by this CardHolder.
+  #
+  # Returns an Array
+  def cards_held
+    @cards_held ||= []
   end
   
+  # Adds a Card to the CardHolder's hand.
   def receive_card(card)
     cards_held << card
   end
   
+  # Sets the CardHolder's hand back to empty.
   def clear_hand
     self.cards_held = []
   end
   
+  # The Cards in the CardHolder's hand that are Aces.
   def aces
     cards_held.select(&:ace?)
   end
   
+  # The total value of the Card's in the CardHolder's hand.
+  # If the total is above BLACKJACK_AMOUNT and the CardHolder has aces, treats the aces
+  # as "1" to reduce the hand value. Otherwise, aces are high.
+  #
+  # Returns an Integer.
   def hand_total
     hand_total_value = cards_held.map(&:game_value).inject(&:+)
     return hand_total_value unless hand_total_value > BLACKJACK_AMOUNT and aces.any?
@@ -32,10 +47,16 @@ module ActsAsCardHolder
     hand_total_value
   end
   
+  # Is the CardHolder's hand greater than BLACKJACK_AMOUNT
+  #
+  # Returns true or false
   def bust?
     hand_total > BLACKJACK_AMOUNT
   end
-  
+
+  # Is the CardHolder's hand equal to BLACKJACK_AMOUNT  
+  #
+  # Returns true or false
   def blackjack?
     hand_total == BLACKJACK_AMOUNT
   end
--- app/dealer.rb
+++ app/dealer.rb
@@ -1,19 +1,25 @@
 class Dealer
   
   require_relative 'acts_as_card_holder'
+
   include ActsAsCardHolder
   
   attr_reader :name
   
   def initialize(name)
-    super
     @name = name
   end
   
+  # Is the Dealer going to "stick" with his current Cards?
+  #
+  # Returns true if the hand_total is between 18 and 21
   def sticking?
     hand_total.between?(18,21)
   end
   
+  # A "hand" for the Dealer with an unturned Card
+  #
+  # Returns an Array of Cards
   def hand_with_hole_card 
     [Card.unturned, cards_held[1]]
   end
