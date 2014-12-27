2ab7c336e7eb43fc00441b604a2f3fe1418285c9
Bodacious <gavin@katanacode.com>
2014-12-27 01:47:49 +0000

    Refactored User-Interaction classes and methods

--- app/game.rb
+++ app/game.rb
@@ -8,7 +8,7 @@ class Game
   attr_accessor :bet_placed
   
   def initialize
-    clear_screen_content
+    UI.clear
     @player = Player.new(player_name, 100)
     @dealer = Dealer.new('Kryton')
     @blackjack_deck = DeckHandler.new
@@ -18,18 +18,14 @@ class Game
   def play
     begin
       if player.amount_in_account == 0
-        TextFormat.print_string LOSING_MESSAGE % player.name 
+        UI.tell(LOSING_MESSAGE % player.name )
         return
       end
       
       run_game_sequence
-
-      begin
-        decision = UserInteraction.new("Would you like to play again?: Y or N").input
-      end while !TextFormat.entered_correct_choice?(decision)
-    end while decision.downcase == 'y'
+    end while UI.ask_yes_no("Would you like to play again?: Y or N") == 'y'
   
-    TextFormat.print_string "Thanks for playing #{player.name}" 
+    UI.tell "Thanks for playing #{player.name}" 
   end
   
   
@@ -37,31 +33,29 @@ class Game
   
   
   def show_cards_on_table(dealer_cards_held, player_cards_held)
-    TextFormat.print_string "The dealers cards are:"
+    UI.tell "The dealers cards are:"
     blackjack_deck.display_cards(dealer_cards_held)
   
-    TextFormat.print_string "#{player.name}'s cards are:"
+    UI.tell "#{player.name}'s cards are:"
     blackjack_deck.display_cards(player_cards_held)
   end
   
   def setup_game
-    TextFormat.print_string "#{player.name} you have £#{player.amount_in_account} in your account. Let's play!"
-    clear_screen_content
+    UI.tell "#{player.name} you have £#{player.amount_in_account} in your account. Let's play!"
+    UI.clear
     fill_game_deck
-    clear_screen_content
+    UI.clear
     player.clear_hand
     dealer.clear_hand
     take_player_bet
-    clear_screen_content
+    UI.clear
     deal_initial_cards
     show_cards_on_table(dealer.hand_with_hole_card, player.cards_held)
   end
   
   def take_player_bet
     begin
-      question = "Place your bet! You have £#{player.amount_in_account} in your account"
-      self.bet_placed = UserInteraction.new(question).input
-      #raise "Bet placed was: #{bet_placed} (#{bet_placed.class})"
+      self.bet_placed = UI.ask("Place your bet! You have £#{player.amount_in_account} in your account")
     end while !MoneyChecker.correct_money_entered?(bet_placed, player.amount_in_account)
   end
   
@@ -72,7 +66,7 @@ class Game
   
   def blackjack_on_first_draw?
     if player.blackjack?
-      TextFormat.print_string "Well done #{player.name}, you have won the game!"
+      UI.tell "Well done #{player.name}, you have won the game!"
       player.win_money(bet_placed.to_i)
       true 
     end
@@ -84,29 +78,23 @@ class Game
       dealer.hand_with_hole_card
       show_cards_on_table(dealer.hand_with_hole_card, player.cards_held)
       break if player.bust?
-    
-      begin
-        question = "#{player.name}, would you like another card? Y or N"
-        decision = UserInteraction.new(question).input
-      end while !TextFormat.entered_correct_choice?(decision)
-    
-      if decision.downcase == 'y'
-        clear_screen_content
+      decision = UI.ask_yes_no("#{player.name}, would you like another card? Y or N")
+      if decision == 'y'
+        UI.clear
         player.receive_card(blackjack_deck.deal_card)
       end
-
-    end while decision.downcase == 'y'
+    end while decision == 'y'
   end
   
   def dealer_round
-    clear_screen_content
+    UI.clear
     loop do
-      TextFormat.print_string "   ********** The dealer is playing... **********"
+      UI.tell "The dealer is playing..."
       show_cards_on_table(dealer.cards_held, player.cards_held)
       break if dealer.sticking?
       break if dealer.bust?
       sleep 2
-      clear_screen_content
+      UI.clear
 
       dealer.receive_card(blackjack_deck.deal_card)
     end 
@@ -115,12 +103,12 @@ class Game
   def declare_result(bet_placed)
     if player_wins?
       player.win_money(bet_placed)
-      TextFormat.print_string "Congratulations #{player.name}, you have won the game!"
+      UI.tell "Congratulations #{player.name}, you have won the game!"
     elsif dealer_wins?
       player.lose_money(bet_placed)
-      TextFormat.print_string "Sorry #{player.name}, the dealer has won the game."
+      UI.tell "Sorry #{player.name}, the dealer has won the game."
     else
-      TextFormat.print_string "It's a tie! Have a go at beating the dealer again #{player.name}."
+      UI.tell "It's a tie! Have a go at beating the dealer again #{player.name}."
     end
   end
   
@@ -137,13 +125,13 @@ class Game
     setup_game
     return if blackjack_on_first_draw?
   
-    clear_screen_content
+    UI.clear
   
     player_round
     
     if player.bust?
       player.lose_money(bet_placed.to_i)
-      TextFormat.print_string "#{player.name} is bust and has lost the game.  #{dealer.name} has won!"
+      UI.tell "#{player.name} is bust and has lost the game.  #{dealer.name} has won!"
       return
     end
 
@@ -151,29 +139,25 @@ class Game
     
     if dealer.bust?
       player.win_money(bet_placed.to_i)
-      TextFormat.print_string "The dealer is bust. #{player.name} has won!" 
+      UI.tell "The dealer is bust. #{player.name} has won!" 
       return
     end
       
     sleep 1
-    clear_screen_content
+    UI.clear
     show_cards_on_table(dealer.cards_held, player.cards_held)
   
     declare_result(bet_placed.to_i)
   end
-  
-  def clear_screen_content
-    puts `clear`
-  end
-  
+    
   def player_name
-    UserInteraction.new("Welcome to this game of Blackjack. Please enter your name:").input
+    UI.ask "Welcome to this game of Blackjack. Please enter your name:"
   end
   
   def fill_game_deck
     if blackjack_deck.game_deck.empty? || blackjack_deck.game_deck.size <= 30
       blackjack_deck.replenish_deck(NUMBER_OF_DECKS_FOR_GAME)
-      TextFormat.print_string "...New deck is ready"
+      UI.tell "...New deck is ready"
       sleep 2
     end
   end 
